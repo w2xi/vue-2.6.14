563aa3006be549064f99929e9da40a35f01a1a4e
w2xi <43wangxi@gmail.com>
2022-08-30 18:46:29 +0800

    chore: update

--- src/platforms/web/util/compat.js
+++ src/platforms/web/util/compat.js
@@ -10,7 +10,12 @@ function getShouldDecode (href: boolean): boolean {
   return div.innerHTML.indexOf('&#10;') > 0
 }
 
+// 当前最新的 chrome 浏览器已经没有该 bug
+
+// 如果 shouldDecodeNewlines 为 true，意味着 Vue 在编译模板的时候，要对属性值中的换行符或制表符做兼容处理
 // #3663: IE encodes newlines inside attribute values while other browsers don't
 export const shouldDecodeNewlines = inBrowser ? getShouldDecode(false) : false
+
+// 如果 shouldDecodeNewlinesForHref 为 true 意味着 Vue 在编译模板的时候，要对 a 标签的 href 属性值中的换行符或制表符做兼容处理
 // #6828: chrome encodes content in a[href]
 export const shouldDecodeNewlinesForHref = inBrowser ? getShouldDecode(true) : false
